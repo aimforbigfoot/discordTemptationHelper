171e4a7ed18bc8302b764732d59f782891817107
NAD LAB <61890796+aimforbigfoot@users.noreply.github.com>
2021-10-08 21:13:37 -0300

    universal update 👾

--- src/App.svelte
+++ src/App.svelte
@@ -2,6 +2,9 @@
 	export let totalCount = 0;
 	export let failedCount = 0;
 	export let succeedCount = 0;
+	export let avoidThing = "Thing To Avoid";
+	export let verbToUse = "open";
+	let toShowSettings = true;
 
 	const increaseTotal = () => totalCount++;
 	const increaseFail = () => {
@@ -12,21 +15,31 @@
 		succeedCount++;
 		increaseTotal();
 	}
-
+	const doneSettings = (val) =>{
+		console.log(val, " was  called")
+		toShowSettings = val;
+	}
 </script>
 
 
 <main>
-	<h1>Total times tempted: {totalCount}</h1>
+	<h1>Total times tempted by {avoidThing}: {totalCount}</h1>
 
-	<h2>Times opened Discord: {failedCount}</h2>
-	<h2>Times ignored Discord: {succeedCount}</h2>
+	<h2>Times tempted {avoidThing}: {failedCount}</h2>
+	<h2>Times ignored {avoidThing}: {succeedCount}</h2>
 
 	<div>
-		<button on:click={increaseFail} >I opened Discord</button>
-		<button on:click={increaseSucceed}> I did not open Discord but I wanted to</button>
+		<button on:click={increaseFail} >I {verbToUse}ed {avoidThing}</button>
+		<button on:click={increaseSucceed}> I did not {verbToUse} {avoidThing}</button>
 	</div>
-	
+
+	{#if toShowSettings }
+	<input bind:value={avoidThing} placeholder="what are you trying to avoid" />
+	<input bind:value={verbToUse} placeholder="verb to use?" />
+	<br>
+	<button on:click={() => doneSettings(false)}>Done with settings</button>
+	{/if}
+
 </main>
 
 <style>
@@ -41,6 +54,10 @@
 	}
 	button{
 		padding: 1em;
+	}
+	input{
+		padding: 0.5em;
+		width: 50%;
 	}
 
 </style>
